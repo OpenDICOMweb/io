0bed3352aa96a4f6ef14989e97d25b192d284f6d
Jim Philbin <jfphilbin@gmail.com>
2017-02-14 19:56:11 -0500

    Moving to GitFlow

--- test/dataset/rootset_test.dart
+++ test/dataset/rootset_test.dart
@@ -1,3 +1,8 @@
+// Copyright (c) 2016, Open DICOMweb Project. All rights reserved.
+// Use of this source code is governed by the open source license
+// that can be found in the LICENSE file.
+// Author: Jim Philbin <jfphilbin@gmail.edu> -
+// See the AUTHORS file for other contributors.
 
 import 'dart:io';
 
