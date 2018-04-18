0c01c27dbcd8bc5e2ab9803171ef0c2276f3330a
jfphilbin <jfphilbin@gmail.com>
2018-04-18 11:00:15 -0400

    Dart 2.0.0-dev.48.0 Merged different FileListReaders

--- lib/src/utils/file_list_reader.dart
+++ lib/src/utils/file_list_reader.dart
@@ -6,11 +6,70 @@
 //  Primary Author: Jim Philbin <jfphilbin@gmail.edu>
 //  See the AUTHORS file for other contributors.
 
+import 'dart:collection';
+
 import 'package:core/core.dart';
 import 'package:convert/convert.dart';
 import 'package:io/src/utils/io_utils.dart';
 
 // ignore_for_file: only_throw_errors, avoid_catches_without_on_clauses
+
+class FileListIterator implements Iterator<String> {
+  Map<String, List<String>> fileMap;
+  final Iterable<String> _dirList;
+  int _dirIndex;
+  int _fileIndex;
+  String _currentDir;
+  Iterable<String> _fileList;
+
+  FileListIterator(this.fileMap)
+      : _dirList = fileMap.keys,
+        _dirIndex = 0,
+        _fileIndex = -1 {
+    _currentDir = _dirList.elementAt(_dirIndex);
+    _fileList = fileMap[_currentDir];
+  }
+
+  int get reset {
+    _dirIndex = 0;
+    return _fileIndex = -1;
+  }
+
+  @override
+  String get current {
+    final dir = _currentDir;
+    final file = _fileList.elementAt(_fileIndex);
+    return '$dir$file';
+  }
+
+  @override
+  bool moveNext() {
+    _fileIndex++;
+    if (_fileIndex >= _fileList.length) {
+      _dirIndex++;
+      if (_dirIndex >= _dirList.length) return false;
+      _fileIndex = 0;
+      _currentDir = _dirList.elementAt(_dirIndex);
+      _fileIndex = 0;
+      return true;
+    }
+    return true;
+  }
+}
+
+/// Reads a Map<directory, List<file>>, where directory and file are [String]s.
+class FileMapReader extends IterableBase<String> {
+  final Map<String, List<String>> fileMap;
+
+  FileMapReader(this.fileMap);
+
+  @override
+  FileListIterator get iterator => new FileListIterator(fileMap);
+
+  @override
+  int get length => fileMap.values.length;
+}
+
 
 class FileListReader {
   List<String> paths;
